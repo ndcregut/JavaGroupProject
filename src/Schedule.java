6953d6c0701f2990f02d876eafaf5f7eaff9e8af
DESKTOP-KS9KC69\Nathan <nathan.cregut@trojans.dsu.edu>
2017-07-12 19:55:53 -0500

    Remove the this.datafield lines and replaced them with getters and setters in case we need to add exceptions on the fields.  Added default constructor and made data field private.

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,4 +1,20 @@
 
 public class Schedule {
-
+	int studentID;
+	String semester;
+	int year;
+	int classID;
+	String className;
+	String times;
+	
+	public Schedule(int studentID, String semester, int year,
+			int classID, String className, String times) {
+		this.studentID = studentID;
+		this.semester = semester;
+		this.year = year;
+		this.classID = classID;
+		this.className  = className;
+		this.times = times;
+	}
+	
 }
