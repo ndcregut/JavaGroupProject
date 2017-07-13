8d9f930c2ddf52901d53c364a6175485601d56df
DESKTOP-KS9KC69\Nathan <nathan.cregut@trojans.dsu.edu>
2017-07-12 19:46:29 -0500

    Added Class data and contstructor to Student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,16 @@
 
 public class Student {
-
+	String fName;
+	String lName;
+	int studentID;
+	// String DOB; Don't know what data type you want for this
+	char registrationStatus;
+	public Student(String fName, String lName, int studentID, //String DOB
+			char registrationStatus) {
+		this.fName = fName;
+		this.lName = lName;
+		this.studentID = studentID;
+		//this.DOB = DOB
+		this.registrationStatus = registrationStatus;
+	}
 }
