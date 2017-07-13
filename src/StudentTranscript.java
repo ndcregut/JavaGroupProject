8616f8a6123ccd7a7b12db9da643cb8ef6db81f1
DESKTOP-KS9KC69\Nathan <nathan.cregut@trojans.dsu.edu>
2017-07-12 19:55:53 -0500

    Added data to Schedule and StudentTranscript and constructors

--- src/StudentTranscript.java
+++ src/StudentTranscript.java
@@ -1,4 +1,19 @@
 
 public class StudentTranscript {
-
+	int studentID;
+	String semester;
+	int year;
+	int classID;
+	String className;
+	char grade;
+	
+	public StudentTranscript(int studentID, String semester, int year,
+			int classID, String className, char grade) {
+		this.studentID = studentID;
+		this.semester = semester;
+		this.year = year;
+		this.classID = classID;
+		this.className = className;
+		this.grade = grade;
+	}
 }
