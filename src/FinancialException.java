1de0d2baebcbc7d3ec0d0de8f106658b19aa125c
ongtm <teeamong@gmai.com>
2017-07-24 20:03:05 -0400

    Financial Exception file

---
+++ src/FinancialException.java
@@ -0,0 +1,7 @@
+public class FinancialException extends Exception{
+	
+	//default constructor
+	public FinancialException(){
+		super("There is no Financial information for you at this time.");
+	}
+}
