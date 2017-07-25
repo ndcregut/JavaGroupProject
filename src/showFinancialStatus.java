a032f998a5b51fb6e3eefbdaa7f7c49b500ce3a5
ongtm <teeamong@gmai.com>
2017-07-24 20:52:01 -0400

    Added object creation and try/catch block.  Still needs search and return status methods.

--- src/showFinancialStatus.java
+++ src/showFinancialStatus.java
@@ -1,2 +1,39 @@
-public class showFinancialStatus{
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class showFinancialStatus throws Exception{
+
+	public static void main(String[] args){
+		ArrayList<FinancialStatus> listOfStatus = new ArrayList<>();
+
+		//Data fields for Financial Status
+		int studentID;
+		float accountBalance;
+		String financialStatus;
+
+		//Create new File object with FStatus.txt file
+		java.io.File file = new java.io.File("FStatus.txt");
+
+		//create scanner object for Cars.txt file
+		Scanner input = new Scanner(file);
+
+		//loop to read contents of FStatus
+		while(input.hasNext()){
+			studentID = input.nextInt();
+			accountBalance = input.nextFloat();
+			financialStatus = input.net();
+
+			//use try-catch block to create Finanical Status instance
+			try{
+				listOfStatus.add(new FinancialStatus(studentID, accountBalance, financialStatus));
+			}
+			
+			//prints exception
+			catch(FStatusException){
+			}
+		}
+		
+		//method for searching for student's financial status based on id
+	}
+	
 }
