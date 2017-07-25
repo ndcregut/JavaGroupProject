47d91c45678e23cde855bd693452b92ba6bf6644
ongtm <teeamong@gmai.com>
2017-07-24 22:22:49 -0400

    viewCurrentSchedule intial code, needs search and return method.

--- src/viewCurrentSchedule.java
+++ src/viewCurrentSchedule.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class viewCurrentSchedule{
+
+	public static void main(String[] args) throws Exception{
+
+		//Create new arraylist of Schedule
+		ArrayList<Schedule> listOfSchedule = new ArrayList<>();
+		
+		//data fields for schedule
+		int studentID;
+		String semester;
+		int year;
+		int classID;
+		String className;
+		String times;
+		String building;
+		String room;
+
+		//create new File object for Schedule.txt file
+		java.io.File file = new java.io.File("Schedule.txt");
+
+		//create scanner object for Schedule.txt file
+		Scanner input = new Scanner(file);
+
+		//loop to read contents of Schedule.txt
+		while(input.hasNext()){
+			studentID = input.nextInt();
+			semester = input.next();
+			year = input.nextInt();
+			classID = input.nextInt();
+			className = input.next();
+			times = input.next();
+			building = input.next();
+			room = input.next();
+
+			//Used try-catch blocks to create Schedule instance
+			try{
+				listOfSchedule.add(new Schedule(studentID, semester, year, classID, className, times, building, room));
+			}
+
+			//prints exception
+			catch(ScheduleException){
+			}
+		}
+	}
 }
