024c72a58145af3786bec01b122da50fe1251803
ongtm <teeamong@gmail.com>
2017-08-06 10:58:12 -0400

    Delete ClassException.java

--- src/ClassException.java
+++
@@ -1,9 +0,0 @@
-public class ClassException extends Exception{
-
-	//default constructor
-	public ClassException(){
-		super("This Class is not available at this time.");
-	}
-
-	//Stub for other potenital Class Exceptions as needed
-}
