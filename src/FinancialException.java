59be5e89d090eadceac525ae8a26cc962fc6bf8e
ongtm <teeamong@gmail.com>
2017-08-06 11:00:02 -0400

    Delete FinancialException.java

--- src/FinancialException.java
+++
@@ -1,7 +0,0 @@
-public class FinancialException extends Exception{
-	
-	//default constructor
-	public FinancialException(){
-		super("There is no Financial information for you at this time.");
-	}
-}
